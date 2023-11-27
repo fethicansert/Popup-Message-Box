ff03c3932eb87cbf80005397960d5552963eeb72
fethi <fethisert0@gmail.com>
2023-11-27 20:45:59 +0200

    Nice popUp Message Box I like it

---
+++ index.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.btn');
+const popUp = document.querySelector('.popup');
+const btnOk = document.querySelector('.btn_ok');
+
+
+
+function show(){
+    popUp.classList.toggle('active');
+}
